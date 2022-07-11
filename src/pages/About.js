de79d208e98b64c8192056b49898ea2b9bcadf9a
SziNo <szino1848@gmail.com>
2022-07-11 12:54:02 +0200

    Captain Morgan ftw

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        porro a suscipit ea sit possimus distinctio beatae optio vel, ipsam ut
+        atque molestias inventore earum aperiam recusandae dolore ipsa non
+        praesentium, veniam iste mollitia maiores doloremque laudantium.
+        Recusandae, error dolorem.
+      </p>
+    </section>
+  )
+}
+
+export default About
